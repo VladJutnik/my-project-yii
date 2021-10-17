ac12a7ceb32d450eab28051acb02181aaf36b438
VladJutnik <v2517v@gmail.com>
2021-10-17 22:22:29 +0300

    Готова симуляция пользователей

--- backend/assets/AppAsset.php
+++ backend/assets/AppAsset.php
@@ -15,20 +15,20 @@ class AppAsset extends AssetBundle
         'css/site.css',
         'assets-custom/plugins/metismenu/css/metisMenu.min.css',
         'assets-custom/css/pace.min.css',
-        'assets-custom/js/pace.min.js',
+        //'assets-custom/js/pace.min.js',
         'assets-custom/css/bootstrap.min.css',
         'assets-custom/css/icons.css',
         'assets-custom/css/app.css',
-        'assets-custom/css/dark-sidebar.css',
-        'assets-custom/css/dark-theme.css',
+        //'assets-custom/css/dark-sidebar.css',
+        //'assets-custom/css/dark-theme.css',
     ];
     public $js = [
-        'assets-custom/js/popper.min.js',
+        //'assets-custom/js/popper.min.js',
         'assets-custom/js/bootstrap.min.js',
         'assets-custom/js/app.js',
         'assets-custom/plugins/metismenu/js/metisMenu.min.js',
-        'assets-custom/plugins/simplebar/js/simplebar.min.js',
-        'assets-custom/plugins/perfect-scrollbar/js/perfect-scrollbar.js',
+        //'assets-custom/plugins/simplebar/js/simplebar.min.js',
+        //'assets-custom/plugins/perfect-scrollbar/js/perfect-scrollbar.js',
     ];
     public $depends = [
         'yii\web\YiiAsset',
