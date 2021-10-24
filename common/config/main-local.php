4d56db612704d6a5634957da559d6572db420833
VladJutnik <v2517v@gmail.com>
2021-10-24 19:44:43 +0300

    Печать по магазину готова

--- common/config/main-local.php
+++ common/config/main-local.php
@@ -11,15 +11,8 @@ return [
         ],
         'mailer' => [
             'class' => 'yii\swiftmailer\Mailer',
-            'transport' => [
-                'class' => 'Swift_SmtpTransport',
-                'host' => 'smtp.yandex.ru',
-                'username' => '1@niig.su',
-                'password' => '',
-                'port' => 465,
-                'encryption' => 'ssl',
-            ],
-            'useFileTransport' => false,
+            'viewPath' => '@common/mail',
+            'useFileTransport' => true,
         ],
     ],
 ];
